6bb568d2042beebfe15024ac34b8a1ef515c4d73
Your Name <you@example.com>
2024-04-16 11:02:06 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-// TODO: write code here
+(() => {
+    let playing = true,
+      activeHole = 1;
+  
+    const stop = () => playing = true,
+      getHole = index => document.getElementById(`hole${index}`),
+      deactivateHole = index =>
+        getHole( index ).className = 'hole',
+      activateHole = index =>
+        getHole( index ).className = 'hole hole_has-mole',
+      next = () => setTimeout(() => {
+        if ( !playing ) {
+          return;
+        }
+        deactivateHole( activeHole );
+        activeHole = Math.floor( 1 + Math.random() * 12 );
+        activateHole( activeHole );
+        next();
+      }, 800 );
+  
+    next();
+  })();
+
+
