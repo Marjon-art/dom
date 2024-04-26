ae782e39b92d1f12b2a287c296f9f96585495a42
Your Name <you@example.com>
2024-04-26 11:51:16 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,4 @@
-(() => {
-    let playing = true,
-      activeHole = 1;
-  
-    const stop = () => playing = true,
-      getHole = index => document.getElementById(`hole${index}`),
-      deactivateHole = index =>
-        getHole( index ).className = 'hole',
-      activateHole = index =>
-        getHole( index ).className = 'hole hole_has-mole',
-      next = () => setTimeout(() => {
-        if ( !playing ) {
-          return;
-        }
-        deactivateHole( activeHole );
-        activeHole = Math.floor( 1 + Math.random() * 12 );
-        activateHole( activeHole );
-        next();
-      }, 800 );
-  
-    next();
-  })();
-
+const fields = document.querySelectorAll(".fields");
+const img = document.createElement("img");
+img.src = "../img/goblin.png";
 
