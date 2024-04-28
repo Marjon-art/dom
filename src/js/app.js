a4bf5a77ea9aa834eabce0592048923f39b19843
Your Name <you@example.com>
2024-04-28 19:34:38 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
+import '../css/style.css'
+import imgGame from '../img/goblin.png'
 const fields = document.querySelectorAll(".fields");
 const img = document.createElement("img");
-img.src = "../img/goblin.png";
 
+img.src = imgGame;
+fields[0].append(img);
+
+
+
+
+// добавь картинку в ячейку поля, кодом 
+//метод Math.random отработал, вернул, например, 10. 
+//Подставь вместо Math.random() в выражение - Math.random().fields[6].append(img); - эту 10. 
+//Прочитай что получилось, как это будет работать 
