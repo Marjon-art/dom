0dd3a48e0e3e3184c2914dd490bd40ea83180412
Verda Muso <gearmobile@gmail.com>
2024-05-15 20:03:15 +0300

    refactor game

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,41 @@
-import '../css/style.css'
-import imgGame from '../img/goblin.png'
-const fields = document.querySelectorAll(".fields");
-const img = document.createElement("img");
+const boardSize = 4;
 
+/** Генерируем таблицу */
+const field = document.createElement("div");
+field.className = "field";
 
-img.src = imgGame;
-fields[0].append(img);
-const number = randomA;
+for (let i = 0; i < boardSize * boardSize; i += 1) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  field.appendChild(cell);
+}
+document.body.appendChild(field);
 
-  function demo() { 
-    function getRandom(max) {
-    let randomA = Math.floor(Math.random() * max);
-    while (randomA === number) {
-      randomA = Math.floor(Math.random() * max);
-          } 
-          number = randomA; 
-          return number; 
-        }
-    }
-    
-    //const interval = setInterval(() => demo(), 1000);
+/** Получаем список всех ячеек таблицы */
+const cells = Array.from(document.querySelectorAll(".cell"));
 
+/** Функция удаления и добавления изображения в ячейку при помощи CSS-класса */
+function goblinImg(index, number) {
+  if (index !== -1) {
+    cells[index].classList.remove("goblin");
+  }
 
+  const goblinImgCell = cells[number];
+  goblinImgCell.classList.add("goblin");
+}
 
+/** Запускаем игру */
+setInterval(() => {
+  /** Находим в выбранном массиве ячеек - индекс ячейки с изображением в ней */
+  const index = cells.findIndex((item) => item.className.includes("goblin"));
 
+  /** Генерируем случайное число */
+  let number = Math.floor(Math.random() * cells.length);
 
-
-
-
-
-
-
-
-
-
-
-// добавь картинку в ячейку поля, кодом 
-//метод Math.random отработал, вернул, например, 10. 
-//Подставь вместо Math.random() в выражение - Math.random().fields[6].append(img); - эту 10. 
-//Прочитай что получилось, как это будет работать 
+  /** Проверяем, равно ли случайное число индексу ячейки */
+  if (number === index) {
+    number = Math.floor(Math.random() * cells.length);
+  } else {
+    goblinImg(index, number);
+  }
+}, 1000);
